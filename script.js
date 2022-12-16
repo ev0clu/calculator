ee4dfaa8e85ac05625cd320052864d160a1f2fcb
ev0clu <ev0clu@gmail.com>
2022-12-16 19:52:23 +0100

    Script is prepared to operate with buttons

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //--------- Variable declarations  ---------//
-
+let displayLowerValue = "";
+let displayUpperValue = "";
 
 //--------- Functions declaration  ---------//
 function add(a, b){
@@ -33,11 +34,53 @@ function operate(a, b, operator){
 
 
 //--------- Query Selectors ---------//
-
+const buttons = document.querySelectorAll("button");
+const displayUpper = document.querySelector(".display-upper");
+const displayLower= document.querySelector(".display-lower");
 
 
 //--------- Set default values ---------//
-
+displayLowerValue = "0";
+displayLower.textContent = displayLowerValue;
 
 
 // --------- Event listeners ---------//
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if(button.classList.contains("btn-plusminus")){
+    }
+
+
+    else if(button.classList.contains("btn-dot")){
+    }
+
+
+    else if(button.classList.contains("btn-backspace")){
+    }
+
+
+    else if(button.classList.contains("btn-clear")){
+    }
+
+
+    else if(button.classList.contains("btn-plus")){
+    }
+
+    else if(button.classList.contains("btn-minus")){
+    }
+
+    else if(button.classList.contains("btn-multiply")){
+    }
+
+    else if(button.classList.contains("btn-divide")){
+    }
+
+
+    else if(button.classList.contains("btn-equal")){    
+    }
+
+    // If "number" buttons are pressed, than add to displayLowerValue
+    else {
+    }
+  });
+});
